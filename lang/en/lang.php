a239b6f8419d9152327984cf1aac595275e3bc68
Sascha Aeppli <munxar@gmx.de>
2017-03-27 14:30:41 +0200

    FFDE-80, FFDE-87 renamed movies to films

---
+++ lang/en/lang.php
@@ -0,0 +1,17 @@
+<?php return [
+    'plugin' => [
+        'name' => 'Films',
+        'description' => 'FFTE Films Plugin.',
+    ],
+    'movies' => [
+        'title' => 'Films',
+    ],
+    'movie' => [
+        'production' => [
+            'comment' => 'add people & companies',
+        ],
+    ],
+    'people' => [
+        'title' => 'People & Companies',
+    ]
+];
